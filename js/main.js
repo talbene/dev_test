cffd547026d2bfec93cc90792afbbd2ee8ee1997
Talbene <talbene@gmail.com>
2015-11-11 17:53:10 +0200

    ajax

--- js/main.js
+++ js/main.js
@@ -1,48 +1,61 @@
 
 
 
+function postData(e) {
 
-document.addEventListener("DOMContentLoaded", function(){
-    var form = document.querySelector("form")
+    // validation
 
-    form.addEventListener("submit",postData);
-});
-
-function postData(formsubmission){
+    var error = false;
 
     var name = document.querySelector(".name").value;
     var email = document.querySelector(".email").value;
     var subject = document.querySelector(".subject").value;
-    var message =document.querySelector(".mainArea").value;
+    var message = document.querySelector(".mainArea").value;
+    var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 
-    // Checks if fields are filled-in or not, returns response "<p>Please enter your details.</p>" if not.
-    if(name == "" || email == "" || subject == "" || message == ""){
-        document.querySelector(".response").innerHTML = "Please enter your details.";
-        return;
+    // Checks if fields are filled-in or not
+    if ((name == "" || email == "" || subject == "" || message == "")) {
+        document.querySelector(".response").style.visibility = "visible";
+        error = true;
+        e.preventDefault()
     }
 
+    if (((!filter.test(document.querySelector(".email").value)))) {
+        alert('Please provide a valid email address');
+        error = true;
+    }
 
-    // Parameters to send to PHP script.
-    var params = "name=" + name + "&email=" + email + "&subject=" + subject + "&message=" + message;
-    console.log(params)
-    var http = new XMLHttpRequest();
+   if(error===false) {
+       // ajax submit
+       // Parameters to send to PHP script.
+        var params = "name=" + name + "&email=" + email + "&subject=" + subject + "&message=" + message;
 
-    http.open("POST","server.php",true);
+        console.log(params);
 
-    // Set headers
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        var http = new XMLHttpRequest();
+        http.open("POST", "server.php", true);
 
+        // Set headers
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-    http.onreadystatechange = function(){
-        if(http.readyState == 4 && http.status == 200){
-            document.querySelector(".response").innerHTML = http.responseText;
-        }
+        http.onreadystatechange = function () {
 
+            if (http.readyState == 4 && http.status == 200) {
 
-    };
-    http.send(params);
+                document.querySelector(".response").innerHTML = http.responseText;
+            }
+        };
+
+        http.send(params);
+
+        alert("success")
+    }
 
 }
+
+
+var form = document.querySelector("form");
+form.addEventListener("submit",postData);
 
 
 
